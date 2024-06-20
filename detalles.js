126437890f699b9523ed0f7e28ac812f31ec36e7
katherine-j-c-s <kathijcs@gmail.com>
2024-06-20 18:35:45 -0300

    finished data of details

---
+++ detalles.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const serviceId = parseInt(urlParams.get('id'));
+
+    // Función para actualizar los elementos en la página de detalles
+    function actualizarDetalles() {
+        //busca en la data de servicios el objeto del id por la posicion
+        const servicio = servicios[serviceId];
+
+        // Actualizar el título y subtitulo
+        document.querySelector('.title h2').textContent = servicio.titulo;
+        document.querySelector('.title p').textContent = servicio.subtitulo;
+
+        // Actualizar el precio y detalle
+        document.getElementById('precio').textContent = servicio.precio;
+        document.getElementById('detalle').textContent = servicio.detalle;
+
+        // Actualizar las imágenes del carrusel
+        const slides = document.querySelectorAll('.carousel .slide img');
+        slides.forEach((img, index) => {
+            img.src = servicio.imagenes[index];
+        });
+    }
+
+    // Llamar a la función para actualizar los detalles
+    actualizarDetalles();
+});
---
+++ home.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Obtener todos los elementos de servicio
+    const serviceCards = document.querySelectorAll('.service-card');
+
+    // Función para manejar el clic en los servicios
+    function handleServiceClick(event) {
+        // Obtener el ID del servicio desde el atributo data-id
+        const serviceId = parseInt(event.currentTarget.getAttribute('data-id'));
+
+        // Redirigir a la página de detalles.html y pasar los datos
+        window.location.href = `detalles.html?id=${serviceId}`;
+    }
+
+    // Agregar un evento de clic a cada servicio
+    serviceCards.forEach(card => {
+        card.addEventListener('click', handleServiceClick);
+    });
+});
